d7bdfa6a29e7ead5ea7011d7fdf43cafe098326a
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-06-15 15:16:00 +0000

    M package.json  D src/ProxEffectReact.tsx  M src/index.ts  D src/proxEffect.ts  M src/useProxEffect.tsx  M yarn.lock

--- src/useProxEffect.tsx
+++ src/useProxEffect.tsx
@@ -1,1 +1,29 @@
- 
+import proxEffect from './index'
+import { useEffect, useState, MutableRefObject, RefObject } from 'react'
+
+export type HTMLElementRef =
+  | MutableRefObject<HTMLElement | undefined | null>
+  | RefObject<HTMLElement | undefined | null>
+
+export default function useProxEffect(
+  anchorRef: HTMLElementRef,
+  containerRef?: HTMLElementRef
+) {
+  const [degree, setDegree] = useState(0)
+
+  useEffect(() => {
+    if (!anchorRef?.current) {
+      throw new Error('Unable to get anchorRef.current')
+    }
+
+    const effect = proxEffect({
+      handler: setDegree,
+      anchor: anchorRef.current,
+      container: containerRef?.current || undefined,
+    })
+
+    return effect.abort
+  }, [])
+
+  return degree
+}
